dcb9e4063b60d25cfd0daf6afe9d405ea7d23652
Paravojik <muhaloiko.vitalik@gmail.com>
2023-05-21 16:00:15 +0300

    added json

--- src/Components/MainPage/MainSkils/MainSkils.jsx
+++ src/Components/MainPage/MainSkils/MainSkils.jsx
@@ -1,21 +1,13 @@
+import { useMemo } from 'react'
 import './MainSkils.css'
 import SkillBox from './SkillBox'
+import skilsJson from './skills.json'
 
-
-// import html from './img/html.png'
-// import css from './img/css.png'
-// import react from './img/react.png'
 function MainSkils(props){
-    // let [arrOfSkils,setArrOfSkils]=useState([
-    //     {id:0,name:'html'},
-    //     {id:1,name:'css'},
-    //     {id:2,name:'react'},
-    // ])
-    let arrOfSkils=([
-        {id:0,date:'02/2020',img:'https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png',info:`It's basic technology that I learned in Creator It Academy. It took me nearly half year.`,name:'HTML'},
-        {id:1,img:'none',name:'CSS'},
-        {id:2,img:'none',name:'React'},
-    ])
+
+    let arrOfSkils=useMemo(()=>skilsJson,[])
+    console.log(arrOfSkils)
+
  
 
    return(
